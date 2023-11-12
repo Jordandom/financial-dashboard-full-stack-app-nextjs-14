ec42c464220afb84e3c57a93e109f47c04d5053b
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-11-12 20:31:03 +0100

    working app

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import AcmeLogo from "@/app/ui/acme-logo"
-import { ArrowRightIcon } from "@heroicons/react/24/outline"
-import { lusitana } from "@/app/ui/fonts"
-import Link from "next/link"
-import Image from "next/image"
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page() {
   return (
@@ -15,7 +15,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -29,22 +29,23 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
+            className="hidden md:block"
           />
           <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className="block md:hidden"
             alt="Screenshot of the dashboard project showing mobile version"
+            className="block md:hidden"
           />
         </div>
       </div>
     </main>
-  )
+  );
 }
